fd569a0a2ad07869b2c5aa24382d8dc6d1ddd08e
pfphilly <126019714+pfphilly@users.noreply.github.com>
2023-05-08 11:58:17 +0000

    Setting up CSV

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-import numpy
 import csv
 print("THE HUNGER GAMES")
 
 print("enter the values below 1 to 10")
 print("enter by puting name then stringth then friendlyness then smarts then sponsor popularity separated by commas. when you are done enter 'May The Odds Be Ever In Your Favor' capitalized just like that")
 
-player = ""
 
 
-with open('players.csv', 'r') as players:
-    reader = csv.reader(players)
-for row in reader:
-    print(row)    
+
+with open('players.csv', newline=',,') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        print(row['player'], row['strenght'])  
