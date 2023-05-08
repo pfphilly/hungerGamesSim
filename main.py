d20062f248306ad249b045a409c4d7a4d81490bc
pfphilly <126019714+pfphilly@users.noreply.github.com>
2023-05-08 12:02:44 +0000

    Csv Working

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ print("enter the values below 1 to 10")
 print("enter by puting name then stringth then friendlyness then smarts then sponsor popularity separated by commas. when you are done enter 'May The Odds Be Ever In Your Favor' capitalized just like that")
 
 
+players = []
 
-
-with open('players.csv', newline=',,') as csvfile:
+with open('players.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        print(row['player'], row['strenght'])  
+        players.append(row)
+print(players)
