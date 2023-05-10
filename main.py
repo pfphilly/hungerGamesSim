427d15eff673deb421937531f333526f0ba7e870
pfphilly <126019714+pfphilly@users.noreply.github.com>
2023-05-10 14:59:21 +0000

    the bloodbath is starting

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import csv
 import random
 import numpy
 #make classes for all items
-
-class sword:
+class weapon:
+    owner = ""
+class sword(weapon):
     #1-10
     attack = 7
     defense = 5
@@ -12,7 +13,7 @@ class sword:
     name = "sword"
     actionType = "slashed"
 
-class bow_and_arrows:
+class bow_and_arrows(weapon):
     #1-10
     attack = 8
     defense = 1
@@ -21,14 +22,14 @@ class bow_and_arrows:
     name = "bow and arrows"
     actionType = "shot"
 
-class spear:
+class spear(weapon):
     attack = 7
     defense = 1
     tradeValue = 5
     name = "spear"
     actionType = "impaled"
 
-class net:
+class net(weapon):
     attack = 3
     defense = 3
     tradeValue = 2
@@ -36,7 +37,7 @@ class net:
     actionType = "caught"
     specialEffect = 1
 
-class sling:
+class sling(weapon):
     attack = 5
     defense = 0
     tradeValue = 1
@@ -44,21 +45,21 @@ class sling:
     actionType = "shot"
     specialEffect = 2
 
-class dart:
+class dart(weapon):
     attack = 4
     defense = 0
     tradeValue = 0
     name = "blow darts"
     actionType = "shot"
 
-class knife:
+class knife(weapon):
     attack = 7
     defense = 2
     tradeValue = 3
     name = "knife"
     actionType = "stabbed"
 
-class hatchet:
+class hatchet(weapon):
     attack = 8
     defense = 3
     tradeValue = 5
@@ -75,7 +76,17 @@ knife5 = knife()
 knife6 = knife()
 knife7 = knife()
 knife8 = knife()
-
+bow_and_arrows1 = bow_and_arrows()
+bow_and_arrows2 = bow_and_arrows()
+spear1 = spear()
+spear2 = spear()
+darts1 = dart()
+darts2 = dart()
+sling1 = sling()
+sling2 = sling()
+sword1 = sword()
+sword2 = sword()
+sword3 = sword()
 print("THE HUNGER GAMES")
 
 #print("enter the values below 1 to 10")
@@ -95,7 +106,8 @@ players = {
     'avni' : {'name' : 'avni', 'strength' : 2, 'friendlyness' : 9, 'smarts' : 10, 'sponsor_popularity' : 7, 'special_trait' : 5},
     'izzy' : {'name' : 'izzy', 'strength' : 7, 'friendlyness' : 8, 'smarts' : 8, 'sponsor_popularity' : 9, 'special_trait' : 4},
     'luca' : {'name' : 'luca', 'strength' : 8, 'friendlyness' : 6, 'smarts' : 10, 'sponsor_popularity' : 1, 'special_trait' : 18},
-    'alden' : {'name' : 'alden', 'strength' : 10, 'friendlyness' : 6, 'smarts' : 9, 'sponsor_popularity' : 8, 'special_trait' : 19 }
+    'alden' : {'name' : 'alden', 'strength' : 10, 'friendlyness' : 6, 'smarts' : 9, 'sponsor_popularity' : 8, 'special_trait' : 19 },
+    'james H' : {'name' : 'james H', 'strength' : 6, 'friendlyness' : 10, 'smarts' : 3, 'sponsor_popularity' : 5, 'special_trait' : 18}
 }
 print("LET THE GAMES BEGIN")
 
@@ -110,9 +122,27 @@ for player in players:
 
 print("BLOODBATH:\n")
 bloodbath = []
-cornicopiaWeapons = numpy.array['hatchet', 'hatchet', 'knife', 'knife', 'knife', 'knife', 'knife', 'knife', 'knife', 'knife', 'bowArrows', 'bowArrows', 'handgun', 'spear', 'spear', 'darts', 'darts', 'battle axe', 'sling', 'sling', 'sword', 'sword', 'sword']
-cornicopiaItems = numpy.array['medkit', 'medkit', 'medkit', 'largeRation', 'largeRation', 'smallRation', 'smallRation', 'smallRation', 'smallRation', 'smallRation', 'tarp', 'tarp', 'tarp', 'tarp', 'tarp', 'tarp', 'backpackLarge', 'backpackSmall', 'backpackSmall']
+cornicopiaWeapons = numpy.array([hatchet1, hatchet2, knife1, knife2, knife3, knife4, knife5, knife6, knife7, knife8, bow_and_arrows1, bow_and_arrows2, spear1, spear2, darts1, darts2, sling1, sling2, sword1, sword2, sword3])
+cornicopiaWeaponsCatalog = numpy.array(cornicopiaWeapons)
+cornicopiaItems = numpy.array(['medkit', 'medkit', 'medkit', 'largeRation', 'largeRation', 'smallRation', 'smallRation', 'smallRation', 'smallRation', 'smallRation', 'tarp', 'tarp', 'tarp', 'tarp', 'tarp', 'tarp', 'backpackLarge', 'backpackSmall', 'backpackSmall'])
 for player in players:
     if players.get(player).get('smarts') < 6 or players.get(player).get('strength') > 7:
         bloodbath.append(player)
-
+        bloodbathBattle = numpy.array(bloodbath)
+random.shuffle(cornicopiaWeapons)
+random.shuffle(bloodbathBattle)
+for player in bloodbathBattle:
+    if players.get(player).get('strength') > 9:
+        cornicopiaWeapons[0].owner = player
+        print(f"{player} grabs a {cornicopiaWeapons[0].name}")
+        numpy.delete(cornicopiaWeapons, 0)
+    if players.get(player).get('strength') > 7:
+        cornicopiaWeapons[0].owner = player
+        print(f"{player} grabs a {cornicopiaWeapons[0].name}")
+        numpy.delete(cornicopiaWeapons, 0)
+    if players.get(player).get('strength') < 7 and players.get(player).get('strength') > 3:
+        cornicopiaWeapons[0].owner = player
+        print(f"{player} grabs a {cornicopiaWeapons[0].name} this should be interesting...")
+        numpy.delete(cornicopiaWeapons, 0)
+    if players.get(player).get('strength') < 3:
+        print("to be continued")
